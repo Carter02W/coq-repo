5409c9a4e48e5ac4718b875490e896ad38bb703a
Carter Wright <wrig0476@algonquinlive.com>
2025-09-24 17:37:13 -0400

    update: minor tweaks to test-api environment connection
    added a path to the env.loval file, and tweaked the response input

--- coq-api/test/test-api.py
+++ coq-api/test/test-api.py
@@ -1,14 +1,15 @@
 from dotenv import load_dotenv
 from openai import OpenAI
 import os
+from pathlib import Path
 
-load_dotenv()  # take environment variables from .env.local
+load_dotenv(dotenv_path=Path(".env.local"))  # take environment variables from .env.local
 
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
 response = client.responses.create(
   model="gpt-5-nano",
-  input="write a haiku about ai",
+  input="how many continents are there on earth?",
   store=True,
 )
 
