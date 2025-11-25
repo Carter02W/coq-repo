f9f2ef969058afa8d61abfcd56e30eaaf9c0ca6d
Carter Wright <wrig0476@algonquinlive.com>
2025-11-25 13:20:34 -0500

    update: explorer layout updates, and new login page added

---
+++ coq-web/app/login/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export default function Home() {
+  return (
+    <div className="flex h-full justify-center bg-blue-800">
+        <button>login</button>
+    </div>
+  );
+};
